0369ab40d37d1d70f3bf53c0807134a4a9b405cf
DevanshDahiya <162833140+DevanshDahiya@users.noreply.github.com>
2025-08-23 21:49:42 +0530

    Add files via upload

---
+++ Day-30/791A-Bear_and_big_brother.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+	int a , b ; 
+	cin>> a>>b ;
+	int ans = 0 ; 
+
+	while(a <=b){
+		a = 3*a;
+        b = 2*b;
+
+        ans++;
+	}
+
+	cout<<ans<<endl;
+
+	return 0 ;
+}
