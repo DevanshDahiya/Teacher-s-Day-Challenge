ce29df1bdbffc4225bc37c55233fe6f6b45c17e7
DevanshDahiya <162833140+DevanshDahiya@users.noreply.github.com>
2025-09-04 10:36:10 +0530

    Add files via upload

---
+++ Day-40/486A-calculating_function.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+	long long n ; 
+	cin>>n ;
+
+	if(n == 1){ 
+		cout<<-1<<endl;
+		return 0 ;
+	} 
+	else if(n % 2 == 0 ){
+		cout<<n/2 <<endl;
+	}
+	else{
+		cout<<-(n-1)/2 ;
+	}
+
+
+	return 0 ;
+}
